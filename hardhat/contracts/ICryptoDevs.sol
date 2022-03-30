1e1186c6e20ab0e6354bf49ba1fe0137ba10867e
Salonii02 <salonisawarkar13@gmail.com>
2022-03-30 19:44:51 +0530

    Changed Smart Contract Note the Difference

--- hardhat/contracts/ICryptoDevs.sol
+++ hardhat/contracts/ICryptoDevs.sol
@@ -1,12 +1,18 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
-interface ICryptoDevs{
-    function tokenofOwnerByIndex(address owner, uint256 index)
-    external 
-    view 
-    returns (uint256 tokenId);
-    function balanceOf(address owner)
-    external 
-    view 
-    returns (uint256 balance);
+
+interface ICryptoDevs {
+    /**
+     * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
+     * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
+     */
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        external
+        view
+        returns (uint256 tokenId);
+
+    /**
+     * @dev Returns the number of tokens in ``owner``'s account.
+     */
+    function balanceOf(address owner) external view returns (uint256 balance);
 }
